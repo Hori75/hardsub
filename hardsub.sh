7b3b0fe87f998b9366a6a42d8fd12f97698659ca
Hori75 <guardianangelhori75@gmail.com>
2017-12-24 13:09:26 +0700

    Add argument, font size

--- hardsub.sh
+++ hardsub.sh
@@ -2,15 +2,23 @@
 
 if [ -z "$*" ]; then
    echo "Usage:";
-   echo "./hardsub.sh <video path> <srt path>";
+   echo "./hardsub.sh <video path> <srt path> [Font size]";
    exit 0;
 elif [ -z "$2" ]; then
    echo "It needs two argument";
-   echo "./hardsub.sh <video path> <srt path>";
+   echo "./hardsub.sh <video path> <srt path> [Font size]";
    exit 0;
-else
+fi
+
 VIDEO="$1"
 SUBTITLE="$2"
-ffmpeg -i $VIDEO -vf subtitles=$SUBTITLE out.avi || { echo 'ffmpeg failed' ; exit 1; }
-exit 0
+
+if [ -z "$3" ]; then
+   ffmpeg -i $VIDEO -vf subtitles=$SUBTITLE out.avi || { echo 'ffmpeg failed' ; exit 1; }
+   exit 0;
 fi
+
+SIZE="$3"
+
+ffmpeg -i $VIDEO -vf subtitles=$SUBTITLE:force_style=Fontsize="$SIZE" out.avi || { echo 'ffmpeg failed' ; exit 1; }
+exit 0;
