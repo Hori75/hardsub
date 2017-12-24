86364624a0ee559d394403378c91c28c4822adef
Hori75 <guardianangelhori75@gmail.com>
2017-12-24 17:57:47 +0700

    Creating options and the documentation of it

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Automates ffmpeg for burning subtitle purposes
 # Usage
 
 - Run `./install.sh` to install its dependencies
-- Run `./hardsub.sh <video path> <srt path>` to run it
-- You can set the font size with `./hardsub.sh <video path> <srt path> [font size]`
+- Run `./hardsub.sh <video path> <srt path> [OPTIONS]` to run it
 - It outputs an .avi file in hardub folder
+
+# Options
+
+- `--size=`   : Configures the font size
+- `--font=`   : Configures the font type
+- `--colour=` : Configures the font colour (in hexadecimal in Blue Green Red order)
--- hardsub.sh
+++ hardsub.sh
@@ -2,11 +2,19 @@
 
 if [ -z "$*" ]; then
    echo "Usage:";
-   echo "./hardsub.sh <video path> <srt path> [Font size]";
+   echo "./hardsub.sh <video path> <srt path> [OPTIONS]";
+   echo "OPTIONS:";
+   echo " --size=     : Configures the font size";
+   echo " --colour=   : Configures the font colour (hexadecimal in Blue Green Red order)";
+   echo " --font=     : Configures the type of font used";
    exit 0;
 elif [ -z "$2" ]; then
    echo "It needs two argument";
-   echo "./hardsub.sh <video path> <srt path> [Font size]";
+   echo "./hardsub.sh <video path> <srt path> [FOPTIONS]";
+   echo "OPTIONS:";
+   echo " --size=     : Configures the font size";
+   echo " --colour=   : Configures the font colour (hexadecimal in Blue Green Red order)";
+   echo " --font=     : Configures the type of font used";
    exit 0;
 fi
 
@@ -18,21 +26,56 @@ if [ -z "$3" ]; then
    exit 0;
 fi
 
-SIZE="$3"
+case "$3" in
+   "--size="*)
+      SIZE=${3:7}
+    ;;
+    "--colour="*)
+      COLOUR=${3:9}
+    ;;
+    "--font="*)
+      FONT=${3:7}
+    ;;
+esac
 
-if [ -z "$4"  ]; then
-   ffmpeg -i $VIDEO -vf subtitles=$SUBTITLE:force_style=Fontsize="$SIZE" out.avi || { echo 'ffmpeg failed' ; exit 1; }
-   exit 0;
+case "$4" in
+   "--size="*)
+      SIZE=${4:7}
+    ;;
+    "--colour="*)
+      COLOUR=${4:9}
+    ;;
+    "--font="*)
+      FONT=${4:7}
+    ;;
+esac
+
+case "$5" in
+   "--size="*)
+      SIZE=${5:7}
+    ;;
+    "--colour="*)
+      COLOUR=${5:10}
+    ;;
+    "--font="*)
+      FONT=${5:8}
+    ;;
+esac
+
+if [  -z "$SIZE" ] && [ -z "$COLOUR"  ]; then
+   ffmpeg -i $VIDEO -vf "subtitles=$SUBTITLE:'force_style=FontName="$FONT"'" out.avi || { echo 'ffmpeg failed' ; exit 1; }
+elif [  -z "$SIZE" ] && [ -z "$FONT"  ]; then
+   ffmpeg -i $VIDEO -vf "subtitles=$SUBTITLE:'force_style=PrimaryColour="$COLOUR"'" out.avi || { echo 'ffmpeg failed' ; exit 1; }
+elif [  -z "$FONT" ] && [ -z "$COLOR"  ]; then
+   ffmpeg -i $VIDEO -vf "subtitles=$SUBTITLE:'force_style=Fontsize="$SIZE"'" out.avi || { echo 'ffmpeg failed' ; exit 1; }
+elif [ -z "$SIZE" ]; then
+   ffmpeg -i $VIDEO -vf "subtitles=$SUBTITLE:'force_style=FontName="$FONT",PrimaryColour="$COLOUR"'" out.avi || { echo 'ffmpeg failed' ; exit 1; } 
+elif [ -z "$FONT" ]; then
+   ffmpeg -i $VIDEO -vf "subtitles=$SUBTITLE:'force_style=Fontsize="$SIZE",PrimaryColour="$COLOUR"'" out.avi || { echo 'ffmpeg failed' ; exit 1; }
+elif [ -z "$COLOUR" ]; then
+   ffmpeg -i $VIDEO -vf "subtitles=$SUBTITLE:'force_style=Fontsize="$SIZE",FontName="$FONT"'" out.avi || { echo 'ffmpeg failed' ; exit 1; }
+else
+   ffmpeg -i $VIDEO -vf "subtitles=$SUBTITLE:'force_style=Fontsize="$SIZE",FontName="$FONT",PrimaryColour="$COLOUR"'" out.avi || { echo 'ffmpeg failed' ; exit 1; }
 fi
 
-FONT="$4"
-
-if [ -z "$5" ]; then
-   ffmpeg -i $VIDEO -vf subtitles=$SUBTITLE:force_style=Fontsize="$SIZE":force_style=FontName="$FONT" out.avi || { echo 'ffmpeg failed' ; exit 1; }
-   exit 0;
-fi
-
-COLOUR="$5"
-
-ffmpeg -i $VIDEO -vf subtitles=$SUBTITLE:force_style=Fontsize="$SIZE":force_style=FontName="$FONT":force_style=PrimaryColour="$COLOUR" out.avi || { echo 'ffmpeg failed' ; exit 1; }
 exit 0;
